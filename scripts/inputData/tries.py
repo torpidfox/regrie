8fc24f5f7b53cece145b77c75c2215a91a33f84b
Alisa <nonexistence42@gmail.com>
2019-03-25 23:19:18 +0300

    fixing

--- scripts/inputData/tries.py
+++
@@ -1,8 +0,0 @@
-with open("/Users/dmitrav/Politech/Laboratory/StoсhasticModelling/reGRiE2/biodata/Drosophila/genes&chromatin/gt_Raleigh_45.fasta") as file:
-    seq = file.readlines()[1]
-
-print(seq.find("ACCGAAAATC"))
-print(seq[3246:3253])
-print(seq[11958:11958+10])
-
-print(seq[7646:7656])
